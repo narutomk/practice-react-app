0b8e10919410abf3b3d7711d1b383aa1981338a6
narutomk <khaled.bouflija@gmail.com>
2020-10-24 16:53:12 +0100

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
-
+import Nav from './Component/Nav'
 function App() {
+  const [name,setName] = useState('');
+
+  const [compteur,setCompteur] = useState(0);
+  const [show,setshow] = useState(false);
+
+  const incrementCompteur = () => {
+    setCompteur(compteur + 1);
+  }
+  const handleshow = () => {
+    setshow(!show);
+    setName(name);
+  }
+  const handleInput = (e) => {
+    setName(e.target.value)
+  }
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App content ">
+        <div className="d-flex flex-row">
+        <button type = "button" className="btn btn-outline-success p-2" 
+        onClick ={() => {setCompteur(compteur-1)}}>
+          Décréménter</button> 
+
+        <label className="p-2">Mon compteur : {compteur} </label>
+        <button type = "button" className="btn btn-outline-success p-2" onClick ={incrementCompteur}>Incréménter</button> 
+        <button type = "button" className="btn btn-danger p-2" onClick ={() => setCompteur(0)}>Reset Compteur</button> 
+        <input type="text" onChange={handleInput}></input> 
+        <button type = "button" className="btn btn-success p-2" onClick ={handleshow}>Show/hide</button> 
+          
+
+        </div>
+        {show && <Nav username={name} />}
     </div>
   );
 }
---
+++ src/Component/Nav.js
@@ -0,0 +1,24 @@
+import React,{useEffect} from 'react';
+
+const Nav = ({username}) => {
+
+    useEffect(() => {
+        console.log('component did mount !');
+
+        return() => 
+        {
+        console.log('component will unmount !');
+        };
+    },[]);
+ 
+    return (
+        <div>
+                <div className="alert alert-success" role="alert">
+                    hey {username} you just called the Nab component ta-daaaaaaaa !
+                </div>
+
+        </div>
+    )
+}
+
+export default Nav
